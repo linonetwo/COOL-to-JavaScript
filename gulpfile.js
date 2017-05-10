8ab0fa7b811d25d84b744de80215f99ec89fa90a
linonetwo <linonetwo012@gmail.com>
2017-05-10 18:52:40 +0800

    init: boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp')
+const run = require('gulp-run')
+const replace = require('gulp-replace')
+const inlinesource = require('gulp-inline-source')
+const fs = require('fs-promise')
+const del = require('del')
+const Promise = require('bluebird')
+const _ = require('lodash')
+
+gulp.task('clean', function (cb) {
+  return del([
+    './knowledge_modules/programming-languages-and-compilers/build',
+    './build'
+  ])
+})
+
+gulp.task('build-klg', ['clean'], async function () {
+  const basePath = './knowledge_modules/programming-languages-and-compilers'
+
+  await fs.mkdir(`${basePath}/build`)
+  const topics = await fs.readdir(`${basePath}/src`)
+
+  const result = await Promise.reduce(topics, async (resultObject, currentDirName) => {
+    const examplePath = `${basePath}/src/${currentDirName}/example.md`
+    const example = await fs.readFile(examplePath, 'utf8').catch(() => '')
+
+    const principlePath = `${basePath}/src/${currentDirName}/principle.md`
+    const principle = await fs.readFile(principlePath, 'utf8').catch(() => '')
+
+    const tagPath = `${basePath}/src/${currentDirName}/tags.csv`
+    const tags = await fs.readFile(tagPath, 'utf8').catch(() => '')
+
+    resultObject[currentDirName] = {
+      title: currentDirName,
+      tags: _.uniq(tags.length > 0 ? tags.split(',') : []),
+      example,
+      principle
+    }
+
+    return resultObject
+  }, {})
+
+  await fs.writeFile(`${basePath}/build/main.json`, JSON.stringify(result))
+})
+
+gulp.task('build-web', ['build-klg'], function () {
+  return run('npm run build').exec()
+})
+
+gulp.task('build-compiler', function () {
+  return run('antlr -Dlanguage=JavaScript ./src/hello.g4').exec()
+})
+
+gulp.task('inline', ['build-web'], function () {
+  return gulp.src('./build/index.html')
+    .pipe(replace('.js"></script>', '.js" inline></script>'))
+    .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('build', ['inline'])
