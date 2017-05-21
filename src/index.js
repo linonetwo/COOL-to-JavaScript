82a9303f823e6926b2517c043bf16317c606ff1a
linonetwo <linonetwo012@gmail.com>
2017-05-21 14:52:03 +0800

    feat: static-fs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 // @ts-check @flow
-import fs from 'fs-transaction';
-import path from 'path';
+// using babel-plugin-static-fs
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { ParseTreeWalker } from 'antlr4/tree';
 
 import parseCOOL from './parseCOOL';
 import GenerateJSListener from './GenerateJSListener';
 
+const exampleFiles = {
+  // using babel-plugin-static-fs
+  arith: readFileSync(join(__dirname, '../examples/arith.cl'), 'utf8'),
+  atoi: readFileSync(join(__dirname, '../examples/atoi.cl'), 'utf8'),
+  atoi_test: readFileSync(join(__dirname, '../examples/atoi_test.cl'), 'utf8'),
+  list: readFileSync(join(__dirname, '../examples/list.cl'), 'utf8'),
+  book_list: readFileSync(join(__dirname, '../examples/book_list.cl'), 'utf8'),
+  cells: readFileSync(join(__dirname, '../examples/cells.cl'), 'utf8'),
+  cool: readFileSync(join(__dirname, '../examples/cool.cl'), 'utf8'),
+  io: readFileSync(join(__dirname, '../examples/io.cl'), 'utf8'),
+  hairyscary: readFileSync(join(__dirname, '../examples/hairyscary.cl'), 'utf8'),
+  hello_world: readFileSync(join(__dirname, '../examples/hello_world.cl'), 'utf8'),
+  primes: readFileSync(join(__dirname, '../examples/primes.cl'), 'utf8'),
+  graph: readFileSync(join(__dirname, '../examples/graph.cl'), 'utf8'),
+  g1: readFileSync(join(__dirname, '../examples/g1.graph'), 'utf8'),
+  palindrome: readFileSync(join(__dirname, '../examples/palindrome.cl'), 'utf8'),
+  complex: readFileSync(join(__dirname, '../examples/complex.cl'), 'utf8'),
+  life: readFileSync(join(__dirname, '../examples/life.cl'), 'utf8'),
+  sort_list: readFileSync(join(__dirname, '../examples/sort_list.cl'), 'utf8')
+};
+
 async function readFile(): Promise<string> {
-  const filePath: string = path.resolve(process.cwd(), './examples/io.cl');
-  const program: string = await fs.readFile(filePath, 'utf-8');
+  const program: string = exampleFiles['hairyscary'];
   return program;
 }
 
@@ -16,7 +37,11 @@ function visitCOOL(ast) {
   const generateJSListener = new GenerateJSListener();
   const walker = new ParseTreeWalker();
   walker.walk(generateJSListener, ast);
-  console.log(generateJSListener.generateJS());
+  return generateJSListener;
 }
 
-readFile().then(parseCOOL).then(visitCOOL).catch(error => console.error(error));
+function generateJS(generateJSListener) {
+  return generateJSListener.generateJS();
+}
+
+readFile().then(parseCOOL).then(visitCOOL).then(generateJS).then(console.log).catch(error => console.error(error));
