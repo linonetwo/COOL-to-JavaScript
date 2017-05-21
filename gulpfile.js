281bd007bd0dfe630e8b807fb4acb8d5e918b5bf
linonetwo <linonetwo012@gmail.com>
2017-05-21 22:53:48 +0800

    chore: utf-8 support

--- gulpfile.js
+++ gulpfile.js
@@ -14,19 +14,25 @@ gulp.task('clean', function (cb) {
   ]);
 });
 
-gulp.task('build-js', ['clean'], function () {
-  return run('npm run build').exec();
-});
-
 gulp.task('build-compiler', async function () {
   await del('./src/antlrGenerated');
   execSync('java org.antlr.v4.Tool -Dlanguage=JavaScript -o ./src/antlrGenerated -visitor ./src/COOL.g4');
+});
+
+gulp.task('build-js', ['clean'], function () {
+  return run('npm run build').exec();
 });
 
 gulp.task('inline', ['build-js'], function () {
   return gulp.src('./src/COOLRuner.html')
     // .pipe(replace('.js"></script>', '.js" inline></script>'))
     // .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('inline-fast', [ 'clean' ], function () {
+  return gulp.src('./src/COOLRuner.html')
     .pipe(inlinesource())
     .pipe(gulp.dest('./build'));
 });
